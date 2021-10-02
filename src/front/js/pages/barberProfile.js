820ab7c3a87265c23e0085f51fa21bdd90c46b20
fernandoavilesrosado <fernandoavilesrosado@gmail.com>
2021-10-02 16:49:00 +0000

    añado vistas mi cuenta barbero

--- src/front/js/pages/barberProfile.js
+++ src/front/js/pages/barberProfile.js
@@ -3,6 +3,8 @@ import "../../styles/barberProfile.scss";
 import Tab from "react-bootstrap/Tab";
 import Nav from "react-bootstrap/Nav";
 import { Sonnet } from "../component/sonnet.js";
+import { MiCuenta } from "../component/miCuenta.js";
+import logoPositive from "../../img/tonsor-logo-negativo.png";
 //import "bootstrap/dist/css/bootstrap.min.css";
 
 export const BarberProfile = () => {
@@ -27,29 +29,39 @@ export const BarberProfile = () => {
 					</div>
 					<Nav variant="pills" className="flex-column">
 						<Nav.Item>
-							<Nav.Link eventKey="first" onClick={() => toggleTab("first")}>
+							<Nav.Link eventKey="1" onClick={() => toggleTab("1")}>
 								Servicios
 							</Nav.Link>
 						</Nav.Item>
 						<Nav.Item>
-							<Nav.Link eventKey="second">Metodo de pago</Nav.Link>
+							<Nav.Link eventKey="2">Metodo de pago</Nav.Link>
 						</Nav.Item>
 						<Nav.Item>
-							<Nav.Link eventKey="thirt">Facturación</Nav.Link>
+							<Nav.Link eventKey="3">Facturación</Nav.Link>
 						</Nav.Item>
+						<hr size="3" />
+						<Nav.Item>
+							<Nav.Link eventKey="4">Mi Cuenta</Nav.Link>
+						</Nav.Item>
+						<div className="img-profile-logo-container">
+							<img className="img-profile-logo" src={logoPositive} />
+						</div>
 					</Nav>
 				</div>
 
 				<div className="r-menu col-sm-10">
 					<Tab.Content>
-						<Tab.Pane eventKey="first">
+						<Tab.Pane eventKey="1">
 							<Sonnet />
 						</Tab.Pane>
-						<Tab.Pane eventKey="second">
+						<Tab.Pane eventKey="2">
 							<div>Servicios</div>
 						</Tab.Pane>
-						<Tab.Pane eventKey="thirt">
+						<Tab.Pane eventKey="3">
 							<div>Metodo de pago</div>
+						</Tab.Pane>
+						<Tab.Pane eventKey="4">
+							<MiCuenta />
 						</Tab.Pane>
 					</Tab.Content>
 				</div>
@@ -57,48 +69,3 @@ export const BarberProfile = () => {
 		</Tab.Container>
 	);
 };
-/*
-<div className="l-menu">
-				<div className="img-barber-container">
-					<img
-						className="barber_image_profile1"
-						src="https://images.pexels.com/photos/1319460/pexels-photo-1319460.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-						alt=""
-					/>
-				</div>
-				<div className="user-data">
-					<p>Pedro Pérez</p>
-					<hr size="3" />
-				</div>
-				<div>
-					<ul className="mainmenu-list">
-						<li />
-						<li>
-							<a
-								href="offcanvas"
-								data-bs-toggle="offcanvas"
-								role="button"
-								aria-controls="offcanvasExample">
-								Servicios
-							</a>
-						</li>
-						<li>
-							<a href="">Metodo de pago</a>
-						</li>
-						<li>
-							<a href="">Facturación</a>
-						</li>
-						<li>
-							<a href="">Mi cuenta</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<hr size="3" />
-				</div>
-			</div>
-
-			<div className="offcanvas offxanvas-start" tabIndex="-1" id="offcanvas" aria-labelledby="offcanvasExample">
-				hola
-			</div>
-*/
